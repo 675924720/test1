68f68919c6c00ba011ab09218c2f158547cf1c22
lcj <675924720@qq.com>
2020-01-23 14:36:11 +0800

    这是我的第四个功能

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ Page({
     // 使用 wx.createMapContext 获取 map 上下文
 	// 第二个功能
 	// 这个是我们的第三个功能
+	// 这是我们要添加的第四个功能
     this.mapCtx = wx.createMapContext('mMap')
   },
   getCenterLocation: function () {
